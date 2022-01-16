4c97c132a001e5d4d25970b97e63b3625f444d13
LeeHakJoon <2hakjoon@gmail.com>
2022-01-16 19:30:17 +0900

    add stack navigation

--- App.tsx
+++ App.tsx
@@ -10,10 +10,12 @@
 
 import React from 'react';
 import {SafeAreaView, StatusBar, Text, useColorScheme} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Firebase from './screens/walk-record/template/Firebase';
 import GoogleMap from './screens/walk-record/template/GoogleMap';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -22,11 +24,17 @@ const App = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const Stack = createNativeStackNavigator();
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      {/* <GoogleMap /> */}
-      <Firebase />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Login" component={Firebase} />
+          <Stack.Screen name="WalkRecord" component={GoogleMap} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
 };
