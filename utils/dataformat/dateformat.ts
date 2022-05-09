dfdd27d5be9f8eea556cf7fab7a5ec9c68b274e1
HAKJOON0223 <2hakjoon@gmail.com>
2022-05-09 20:15:36 +0900

    bug fix

--- utils/dataformat/dateformat.ts
+++ utils/dataformat/dateformat.ts
@@ -3,12 +3,12 @@ export const now_yyyy_mm_dd = (currentDate = new Date()) => {
 
   date += currentDate.getFullYear() + '-';
 
-  if (currentDate.getMonth() < 9) {
+  if (currentDate.getMonth() <= 9) {
     date += '0';
   }
   date += currentDate.getMonth() + 1 + '-';
 
-  if (currentDate.getDate() < 9) {
+  if (currentDate.getDate() <= 9) {
     date += '0';
   }
   date += currentDate.getDate();
