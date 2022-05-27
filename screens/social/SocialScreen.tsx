942532615b1b18f1946a7f67d739c58451ad8ff0
HAKJOON0223 <2hakjoon@gmail.com>
2022-05-27 20:57:35 +0900

    add error state on webview

--- screens/social/SocialScreen.tsx
+++ screens/social/SocialScreen.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {ActivityIndicator, Alert, StyleSheet, View} from 'react-native';
 import WebView from 'react-native-webview';
 import {mVUserAccessToken, mVUserRefreshToken} from '../../apollo-setup';
+import {colors} from '../../utils/colors';
+import EmotionSadCry from '../components/Icons/EmotionSadCry';
+import TextComp from '../components/TextComp';
 
 function SocialScreen() {
-  console.log('accessToken', mVUserAccessToken());
-  console.log('refreshToken', mVUserRefreshToken());
+  const [onError, setOnError] = useState(false);
+  // console.log('accessToken', mVUserAccessToken());
+  // console.log('refreshToken', mVUserRefreshToken());
 
   const injectJsToWebView = `
     window.localStorage.setItem('USER_ACCESS_TOKEN', '${mVUserAccessToken()}');
@@ -13,16 +18,65 @@ function SocialScreen() {
 
   return (
     <>
-      <WebView
-        source={{uri: 'http://121.154.94.120:4000'}}
-        //source={{uri: 'https://oursocialdog.com'}}
-        style={{height: '100%', width: '100%'}}
-        javaScriptEnabled={true}
-        // userAgent={'SOCIALDOG_APP'}
-        injectedJavaScriptBeforeContentLoaded={injectJsToWebView}
-      />
+      {onError ? (
+        <View style={styles.errorWrapper}>
+          <View style={styles.errorInnerWrapper}>
+            <EmotionSadCry size={70} color={colors.PDarkGray} />
+            <TextComp
+              text={'서버에 연결할 수 없습니다.'}
+              size={20}
+              color={colors.PDarkGray}
+            />
+            <TextComp
+              text={'잠시 후 다시 시도해주세요.'}
+              size={20}
+              color={colors.PDarkGray}
+            />
+          </View>
+        </View>
+      ) : (
+        <WebView
+          source={{uri: 'http://121.154.94.120:4000'}}
+          //source={{uri: 'https://oursocialdog.com'}}
+          style={{height: '100%', width: '100%'}}
+          javaScriptEnabled={true}
+          // userAgent={'SOCIALDOG_APP'}
+          injectedJavaScriptBeforeContentLoaded={injectJsToWebView}
+          renderLoading={() => (
+            <ActivityIndicator
+              size={'large'}
+              color="black"
+              style={{position: 'absolute', width: '100%', height: '100%'}}
+            />
+          )}
+          startInLoadingState={true}
+          onError={syntheticEvent => {
+            const {nativeEvent} = syntheticEvent;
+            setOnError(true);
+            Alert.alert(
+              '오류가 발생했습니다.',
+              '서버에 접속할 수 없습니다. 잠시 후 다시 시도해주세요.',
+            );
+          }}
+        />
+      )}
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  errorWrapper: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  errorInnerWrapper: {
+    height: '20%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
 
 export default SocialScreen;
