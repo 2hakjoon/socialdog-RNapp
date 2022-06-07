27000e0cafaa984d490f93f7f46c369589eaac8d
HAKJOON0223 <2hakjoon@gmail.com>
2022-06-07 20:57:39 +0900

    add movo to join

--- screens/auth/templates/LocalLogin.tsx
+++ screens/auth/templates/LocalLogin.tsx
@@ -7,19 +7,19 @@ import {regexEmail, regexPassword} from '../../../utils/regex';
 import BasicButton from '../../components/BasicButton';
 import {storeData} from '../../../utils/asyncStorage';
 import {USER_ACCESS_TOKEN, USER_REFRESH_TOKEN} from '../../../utils/constants';
-import {useRoute} from '@react-navigation/native';
-import {AuthRouteProp} from '../../../routes';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import {
+  AuthNavigationProp,
+  AuthRouteProp,
+  UseNavigationProp,
+} from '../../../routes';
 import FormInputBox from '../../components/Input/FormInputBox';
 import {
   MLocalLogin,
   MLocalLoginVariables,
 } from '../../../__generated__/MLocalLogin';
-import {mVLoginState} from '../../../apollo-setup';
+import {mVLoginState, mVUserAccessToken} from '../../../apollo-setup';
 import {Alert} from 'react-native';
-
-interface ILogInScreenProps {
-  setAccessToken: Function;
-}
 
 interface ILoginForm {
   email: string;
@@ -37,9 +37,10 @@ const LOGIN = gql`
   }
 `;
 
-function LocalLogin({setAccessToken}: ILogInScreenProps) {
+function LocalLogin() {
   const route = useRoute<AuthRouteProp<'Login'>>();
   const [login] = useMutation<MLocalLogin, MLocalLoginVariables>(LOGIN);
+  const navigation = useNavigation<UseNavigationProp<'AuthTab'>>();
   const {handleSubmit, setValue, formState, control} = useForm<ILoginForm>({
     mode: 'onBlur',
   });
@@ -65,13 +66,17 @@ function LocalLogin({setAccessToken}: ILogInScreenProps) {
     if (res.data?.localLogin && res.data.localLogin.accessToken) {
       await saveTokens(res.data);
       mVLoginState(true);
-      setAccessToken(res.data.localLogin.accessToken);
+      mVUserAccessToken(res.data.localLogin.accessToken);
     }
+  };
+
+  const moveToJoin = () => {
+    navigation.push('Join');
   };
 
   useEffect(() => {
     if (route?.params?.email && route?.params?.password) {
-      console.log(route.params);
+      //console.log(route.params);
       setValue('email', route.params.email);
       setValue('password', route.params.password);
     }
@@ -84,59 +89,68 @@ function LocalLogin({setAccessToken}: ILogInScreenProps) {
 
   return (
     <View style={styles.wrapper}>
-      <FormInputBox
-        titleColor={colors.PWhite}
-        title={'이메일'}
-        name={'email'}
-        control={control}
-        rules={{
-          required: '이메일을 입력해주세요.',
-          pattern: {
-            value: regexEmail,
-            message: '이메일 형식으로 입력해주세요.',
-          },
-        }}
-        errors={formState.errors.email?.message}
-      />
-      <FormInputBox
-        titleColor={colors.PWhite}
-        title={'비밀번호'}
-        name={'password'}
-        control={control}
-        rules={{
-          required: '비밀번호를 입력해주세요',
-          pattern: {
-            value: regexPassword,
-            message:
-              '비밀번호는 최소 8자, 하나 이상의 문자, 하나의 숫자 입니다.',
-          },
-        }}
-        secureTextEntry
-        errors={formState.errors.password?.message}
-      />
-      <BasicButton
-        title="로그인"
-        onPress={handleSubmit(onSumbit)}
-        fontColor={colors.PBlue}
-        fontWeight={'600'}
-        style={styles.buttonStyle}
-      />
-      <BasicButton
-        title="회원가입"
-        onPress={handleSubmit(onSumbit)}
-        fontColor={colors.PBlue}
-        fontWeight={'600'}
-        style={styles.buttonStyle}
-      />
+      <View style={styles.formWrapper}>
+        <FormInputBox
+          titleColor={colors.PWhite}
+          title={'이메일'}
+          name={'email'}
+          control={control}
+          rules={{
+            required: '이메일을 입력해주세요.',
+            pattern: {
+              value: regexEmail,
+              message: '이메일 형식으로 입력해주세요.',
+            },
+          }}
+          errors={formState.errors.email?.message}
+        />
+        <FormInputBox
+          titleColor={colors.PWhite}
+          title={'비밀번호'}
+          name={'password'}
+          control={control}
+          rules={{
+            required: '비밀번호를 입력해주세요',
+            pattern: {
+              value: regexPassword,
+              message:
+                '비밀번호는 최소 8자, 하나 이상의 문자, 하나의 숫자 입니다.',
+            },
+          }}
+          secureTextEntry
+          errors={formState.errors.password?.message}
+        />
+        <BasicButton
+          title="로그인"
+          onPress={handleSubmit(onSumbit)}
+          fontColor={colors.PBlue}
+          fontWeight={'600'}
+          style={styles.buttonStyle}
+        />
+      </View>
+      <View style={styles.joinButtonWrapper}>
+        <BasicButton
+          title="회원가입"
+          onPress={moveToJoin}
+          fontColor={colors.PBlue}
+          fontWeight={'600'}
+          style={styles.buttonStyle}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   wrapper: {
+    height: '100%',
+    width: '100%',
     backgroundColor: colors.PBlue,
     paddingHorizontal: '10%',
-    height: '100%',
+  },
+  formWrapper: {
+    flex: 5,
+    width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -144,6 +158,13 @@ const styles = StyleSheet.create({
     marginTop: 20,
     backgroundColor: colors.PWhite,
     color: colors.PBlue,
+  },
+  joinButtonWrapper: {
+    width: '100%',
+    flex: 1,
+    backgroundColor: colors.PBlue,
+    justifyContent: 'flex-end',
+    paddingBottom: '10%',
   },
 });
 
